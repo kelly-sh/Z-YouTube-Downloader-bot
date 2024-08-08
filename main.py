73a24d5fad27d1cb51b19b44cc6d385d9afcaed9
copperer22 <sergeivaleev86@proton.me>
2024-08-08 13:36:50 +0300

    v1.2 It works!

--- main.py
+++ main.py
@@ -13,16 +13,6 @@ TOKEN = "7299969557:AAF-fRqh_EtwLM6Gcc_sfuVJX5G4U51dT8Y"
 dp = Dispatcher()
 
 
-async def video_download(url, chat_id, message):
-    yt = YouTube(url)
-    stream = yt.streams.get_highest_resolution()
-    stream.download(f'TEMP_{chat_id}', f"{chat_id}_{yt.title}.mp4")
-    with open(f"TEMP_{chat_id}/{chat_id}_{yt.title}.mp4", 'r') as video:
-        await message.answer
-        os.remove(f"TEMP_{chat_id}/{chat_id}_{yt.title}.mp4")
-
-
-
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer("Hello! Send me the link of YouTube video You want to download and I'll try to download It!")

d606194b17f937d33b85b85db4d02479ed746be0
copperer22 <sergeivaleev86@proton.me>
2024-08-15 22:07:29 +0300

    v1.4 Logging added.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from pytubefix import YouTube
 import os
 import logging
 import asyncio
-import sys
 
 TOKEN = "7299969557:AAF-fRqh_EtwLM6Gcc_sfuVJX5G4U51dT8Y"
 dp = Dispatcher()
@@ -27,7 +26,7 @@ async def command_start_handler(message: Message) -> None:
 @dp.message()
 async def main_def(message: Message) -> None:
     chat_id = message.chat.id
-    if message.text.startswith('https://youtu.be' or 'https://www.youtube.com/watch?'):
+    if message.text.startswith("https://youtu.be" or "https://www.youtube.com/watch?"):
         url = str(message.text)
         yt = YouTube(url)
         await message.answer(f"<b>Начинаю загрузку видео</b>: <u>{yt.title}</u> <b>с канала</b>: <u>{yt.author}</u>")
@@ -39,11 +38,35 @@ async def main_def(message: Message) -> None:
         await message.answer("Что-то пошло не так :(\n"
                              "Попробуй ещё раз")
 
+    if message.text.startswith("https://youtu.be" or "https://www.youtube.com/watch?"):
+        yt = YouTube(message.text)
+        video_title = yt.title
+        video_author = yt.author
+        video_url = message.text
+    else:
+        video_title = "NONE"
+        video_author = "NONE"
+        video_url = "NONE"
+
+    logging.info(f"\n"
+                 f"-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+                 f"#[USER INFO]\n"
+                 f"#User's ID = {chat_id}\n"
+                 f"#Username = {message.from_user.username}\n"
+                 f"#User's nickname = {message.from_user.full_name}\n"
+                 f"#Is Premium {message.from_user.is_premium}\n"
+                 f"#[QUERY INFO]\n"
+                 f"#Video title = '{video_title}'\n"
+                 f"#Video author = '{video_author}'\n"
+                 f"#Video URL = {video_url}\n"
+                 f"-----------------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+                 f"\n")
+
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO, filename="logs.log", format="%(levelname)s (%(asctime)s): %(message)s", datefmt="%Y/%m/%d %I:%M:%S", encoding="UTF-8")
     asyncio.run(main())
